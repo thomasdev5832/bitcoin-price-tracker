7474721ab1a63bd593a61c0aa7db153c43782c6c
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-10-17 12:25:33 -0300

    first commit - install tailwindcss with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <main className="max-w-2xl mx-auto px-4 py-8">
+      <h1 className='uppercase text-3xl text-center font-bold bg-gradient-to-br from-orange-500 to-orange-400 bg-clip-text text-transparent'>Find cheapest BTC</h1>
+    </main>
+  )
+}
+
+export default App
