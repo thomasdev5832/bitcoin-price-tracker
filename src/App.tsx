e3956856b851bd8c7c7e95ef1a523292a08a7704
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-10-18 00:08:24 -0300

    add logos and fetches API

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
 import AmountInput from './AmountInput';
 import ResultRow from './ResultRow';
+import axios from 'axios';
+import {sortBy} from 'lodash';
+import  useDebouncedEffect  from  'use-debounced-effect';
+
+type CachedResult = {
+  provider: string;
+  btc: string;
+};
+
+type OfferResults = {
+  [keys:string]:string;
+}
+
+const defaultAmount = '100';
 
 function App() {
+  const [prevAmount, setPrevAmount] = useState(defaultAmount);
   const [amount, setAmount] = useState('100');
+  const [cachedResults, setCachedResults] = useState<CachedResult[]>([]);
+  const [offerResults, setOfferResults] = useState<OfferResults>({});
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    axios.get('https://b1sx1i9xp6.us.aircode.run/cachedValues')
+      .then(res => {
+        setCachedResults(res.data);
+        setLoading(false);
+     });
+  }, []);
+
+  useDebouncedEffect(() => {
+    if(amount === defaultAmount){
+      return;
+    }
+    if(amount !== prevAmount) {
+      setLoading(true);
+      axios
+      .get(`https://b1sx1i9xp6.us.aircode.run/offers?amount=${amount}`)
+      .then(res => {
+        setLoading(false);
+        setOfferResults(res.data);
+        setPrevAmount(amount);
+      })
+    }
+  }, 300, [amount]);
+
+  const sortedCache = sortBy(cachedResults, 'btc').reverse();
+  const sortedResults:CachedResult = sortBy(Object.keys(offerResults).map(provider => ({
+      provider, 
+      btc:offerResults[provider]
+  })), 'btc').reverse();
+
+  const showCached = amount === defaultAmount;
 
   return (
     <main className="max-w-2xl mx-auto px-4 py-8">
@@ -16,10 +66,28 @@ function App() {
         />
       </div>
       <div className='mt-6'>
-        <ResultRow />
-        <ResultRow />
-        <ResultRow />
-        <ResultRow loading={true} />
+        {loading && (
+            <>
+              <ResultRow loading={true} />
+              <ResultRow loading={true} />
+              <ResultRow loading={true} />
+              <ResultRow loading={true} />
+            </>
+        )}
+        {!loading && showCached && sortedCache.map((result:CachedResult) => (
+            <ResultRow 
+              key={result.provider}
+              providerName={result.provider} 
+              btc={result.btc}
+            />
+        ))}
+        {!loading && !showCached && sortedResults.map(result => (
+            <ResultRow 
+            key={result.provider}
+            providerName={result.provider} 
+            btc={result.btc}
+          />
+        ))}
       </div>
     </main>
   )
--- src/ResultRow.tsx
+++ src/ResultRow.tsx
@@ -1,17 +1,52 @@
+import paybisLogo from './assets/Paybis.png';
+import moonpayLogo from './assets/MoonPay.svg';
+import transakLogo from './assets/Transak.svg';
+
+
 type ResultRowProps = {
     loading?: boolean;
+    providerName?: string;
+    btc?: string;
 };
 
-export default function ResultRow({loading}: ResultRowProps) {
+type Logo = {
+    source:string, 
+    invert?:boolean
+};
+
+const logos:{[keys:string]:Logo} = {
+    paybis: {source:paybisLogo, invert:true},
+    moonpay: {source:moonpayLogo},
+    guardarian: {source: 'https://guardarian.com/main-logo.svg'},
+    transak: {source: transakLogo},
+};
+
+export default function ResultRow({loading, providerName, btc}: ResultRowProps) {
     return (
-        <div className=" relative cursor-pointer p-4 m-2 min-h-12 border rounded border-orange-500 bg-neutral-900 hover:bg-orange-500 transition duration-150 ease-out hover:ease-in">
+        <div className=" relative cursor-pointer p-4 m-2 border border-orange-500 rounded  bg-orange-500 hover:bg-neutral-900 hover:border-orange-500 transition duration-150 ease-out hover:ease-in overflow-hidden">
             <div className="flex gap-4">
-                <div>Logo</div>
-                <div className="grow">Provider</div>
-                <div className="flex gap-2">
-                    <span className="font-semibold">0.003</span>
-                    <span className="text-white/80">BTC</span>
-                </div>
+               
+                    {providerName && (
+                        <div className="grow items-center flex">
+                            <img 
+                                src={logos[providerName].source} 
+                                className={
+                                    "w-24 "+(logos[providerName]?.invert ? 'invert' : '')
+                                }
+                                alt="" 
+                            />
+                        </div>
+                    )}
+                
+                
+                {btc && (
+                    <div className="flex gap-2">
+                        <span className="font-semibold">
+                            { new Intl.NumberFormat('sv-SE',{minimumFractionDigits:8}).format(parseFloat(btc))}
+                        </span>
+                        <span className="text-white/80">BTC</span>
+                    </div>
+                )}
             </div>
             {loading && (
                 <div className="inset-0 absolute"></div>
